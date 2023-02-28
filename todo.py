32ba29dca62343d3d6d222ee88e0a0c096455ce7
Aishwarya <libaishwarya@gmail.com>
2023-03-01 00:01:39 +0530

    jwt concept used

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,make_response
 import mysql.connector as mysql
+import jwt
 
 connection = mysql.connect(
   host="localhost",
@@ -9,6 +10,11 @@ connection = mysql.connect(
 )
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+
+data = {
+    "muruga": "this is my todo list muruga",
+    "AMP": "this is test thing"
+}
 
 @app.route('/registerPage', methods=['POST'])
 def registerPage():
@@ -20,7 +26,7 @@ def registerPage():
         cursor.execute("INSERT INTO userDetails(name, emailId,password) VALUES (%s, %s, %s)", (name, emailId, password))
         connection.commit()
         cursor.close()
-        return 'success'
+        return '',200
     
 @app.route("/showList", methods=['GET'])
 def showData():
@@ -29,22 +35,38 @@ def showData():
     data = cursor.fetchall()
     return jsonify(data)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        name = request.form['name']
-        password = request.form['password']
-        cursor = connection.cursor()
-        cursor.execute('SELECT * FROM userDetails WHERE name = %s AND password = %s', (name, password))
-        user = cursor.fetchone()
-    return ({'status':'OK'})
-    # return jsonify(user)
-    # return 'logged in successfully'
-    # return jsonify(user), 200
+    name = request.form['name']
+    password = request.form['password']
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM userDetails WHERE name = %s AND password = %s', (name, password))
+    user = cursor.fetchone()
+    r = make_response(jsonify({
+            "name": user[1],
+            "emailId": user[2]
+        }))
+    r.headers["Authorization"] = jwt.encode({
+            "name": user[1],
+            "emailId": user[2]
+        }, "mysecretkey", algorithm="HS256")
+    return r, 200
+
+@app.route('/getData')
+def getData():
+    authHeader = request.headers.get('Authorization')
+    if authHeader:
+        try:
+            decodedData = jwt.decode(authHeader, key="mysecretkey", algorithms="HS256")
+            return data[decodedData["name"]], 200
+        except:
+            return "", 401
+    else:
+        return "", 200
 
 @app.route('/logout')
 def logout():
     return ('logged out')
      
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
